4aa107700059f649ab5b4ca45eb7d4d4ca17d6fc
Disorol <zzDisorolAnimationzz@gmail.com>
2023-01-24 01:06:20 +0300

    The neuron is ready

--- Neural Network Grams/MainWindow.xaml.cs
+++ Neural Network Grams/MainWindow.xaml.cs
@@ -24,5 +24,28 @@ namespace Neural_Network_Grams
         {
             InitializeComponent();
         }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            var neuron = Neuron.GetNeuron();
+            TextBlock1.Text = neuron.RestoreInputData(decimal.Parse(TextBox1.Text)).ToString() + " кг";
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            var neuron = Neuron.GetNeuron();
+            TextBlock2.Text = neuron.ProcessInputData(decimal.Parse(TextBox2.Text)).ToString() + " г";
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            var neuron = Neuron.GetNeuron();
+            do
+            {
+                neuron.Train(decimal.Parse(TextBox4.Text), decimal.Parse(TextBox3.Text));
+                Console.WriteLine(neuron.LastError);
+            } while (neuron.LastError > neuron.Smoothing || neuron.LastError < -neuron.Smoothing);
+            TextBlock3.Text = "Нейрон обучен!";
+        }
     }
 }
---
+++ Neural Network Grams/Neuron.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neural_Network_Grams
+{
+    class Neuron
+    {
+        private decimal weight = 0.5m;
+        public decimal LastError { get; private set; }
+        public decimal Smoothing { get; set; } = 0.01m;
+
+        private Neuron()
+        {
+
+        }
+
+        private static readonly Neuron _neuron = new Neuron();
+
+        public static Neuron GetNeuron()
+        {
+            return _neuron;
+        }
+
+        public decimal ProcessInputData(decimal input)
+        {
+            return input * weight;
+        }
+
+        public decimal RestoreInputData(decimal output)
+        {
+            return output / weight;
+        }
+
+        public void Train(decimal input, decimal expectedResult)
+        {
+            var actualResult = input * weight;
+            LastError = expectedResult - actualResult;
+            var correction = (LastError / actualResult) * Smoothing;
+            weight += correction;
+        }
+    }
+}
